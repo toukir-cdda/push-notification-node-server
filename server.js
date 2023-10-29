e88222b918a4806f7fa7800987f6850ec08a8be5
Toukir Ahmed Raju <toukir@cdda.io>
2023-10-29 11:23:54 +0600

    update

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const admin = require("firebase-admin");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // Initialize Firebase Admin SDK
 const serviceAccount = require("./serviceAccountKey.json");
@@ -12,21 +14,14 @@ admin.initializeApp({
 });
 
 app.post("/send", (req, res) => {
-  const registrationToken =
-    "enfOxRNJbpNnL1KcIWA99b:APA91bEw2a6fkpo6ntB70aP5b99ccILkz2FEb0SLbFsDw7YBhj3r3gB6oGziFyL4DGr7T0TfrEbpsve-CEbFedzsdcILmNnZ2nR7nfTODJyng7ZhMs7sGxb5LC5JI6JaibuomBc7EEYQ";
-
   const message = {
-    // notification: {
-    //   title: "Hello2 dfds",
-    //   body: "Test notification",
-    // },
     token: req.body.fcmToken,
     data: {
-      openURL: "https://facebook.com/",
+      openURL: req.body.openURL,
       title: req.body.title,
       body: req.body.body,
       image: req.body.image,
-      icon: "https://images.unsplash.com/photo-1574169207511-e21a21c8075a?auto=format&fit=crop&q=80&w=2080&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      icon: req.body.icon,
     },
   };
 
